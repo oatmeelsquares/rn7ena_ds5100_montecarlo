626868c4f0feee8b9e615d4f4944ec8701248c3e
oatmeelsquares <rn7ena@virginia.edu>
2023-12-02 12:08:01 -0500

    update setup

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ setup(
 	description = "DS5100 Final project"
 	url = "https://github.com/oatmeelsquares/rn7ena_ds5100_montecarlo"
 	author = "Becky Desrosiers"
+	description = "README.md"
 	license = "LICENSE.txt"
 	packages = ["montecarlo"]
 	install_requires = ["numpy >= 1.11.1", "pandas >= 2.1"]
