f13439f5558d13d86e7e8f830b8fd28462651512
oatmeelsquares <rn7ena@virginia.edu>
2023-12-02 13:24:00 -0500

    fix setup

--- setup.py
+++ setup.py
@@ -6,7 +6,6 @@ setup(
 	description = "DS5100 Final project",
 	url = "https://github.com/oatmeelsquares/rn7ena_ds5100_montecarlo",
 	author = "Becky Desrosiers",
-	description = "README.md",
 	license = "LICENSE.txt",
 	long_description = "README.md",
 	packages = ["montecarlo"],
