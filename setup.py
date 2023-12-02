81ecea12f819caee0583895290a6ca07df6d8322
oatmeelsquares <rn7ena@virginia.edu>
2023-12-02 11:23:56 -0500

    update 12/2/23

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+	name = "Monte Carlo Simulator"
+	version = "0.1""
+	description = "DS5100 Final project"
+	url = "https://github.com/oatmeelsquares/rn7ena_ds5100_montecarlo"
+	author = "Becky Desrosiers"
+	license = "MIT"
+	packages = ["montecarlo"]
+)
