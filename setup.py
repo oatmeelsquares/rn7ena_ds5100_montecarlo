63bb01daf0f2563555fd23ab02a865d9ae119188
oatmeelsquares <rn7ena@virginia.edu>
2023-12-02 13:08:11 -0500

    update setup

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
 from setuptools import setup
 
 setup(
-	name = "Monte Carlo Simulator"
-	version = "0.1""
-	description = "DS5100 Final project"
-	url = "https://github.com/oatmeelsquares/rn7ena_ds5100_montecarlo"
-	author = "Becky Desrosiers"
-	description = "README.md"
-	license = "LICENSE.txt"
-	packages = ["montecarlo"]
-	install_requires = ["numpy >= 1.11.1", "pandas >= 2.1"]
+	name = "Monte Carlo Simulator",
+	version = "0.1",
+	description = "DS5100 Final project",
+	url = "https://github.com/oatmeelsquares/rn7ena_ds5100_montecarlo",
+	author = "Becky Desrosiers",
+	description = "README.md",
+	license = "LICENSE.txt",
+	long_description = "README.md",
+	packages = ["montecarlo"],
+	install_requires = ["numpy >= 1.11.1", "pandas >= 2.1"],
 )
